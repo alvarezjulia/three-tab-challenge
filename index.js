483847be053631a1ca6e3d88c6bb9a2f6b426328
Julia Álvarez <alvarezjulia1@yahoo.es>
2019-07-03 15:52:08 +0200

    toggle nav bar

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 window.addEventListener("load", function () {
-    let anchors = document.querySelectorAll("a")
-    let idsDiv = document.querySelectorAll(".tab-pane")
-    function myTabClicks(tabClickEvent) {
-        for (var i = 0; i < anchors.length; i++) {
-            idsDiv[i].classList.remove("active")
-        }
-        let clickedTab = tabClickEvent.currentTarget
-        let id = clickedTab.getAttribute("href")
-        document.getElementById(id).classList.add("active")
-    }
-    for (i = 0; i < anchors.length; i++) {
-        anchors[i].addEventListener("click", myTabClicks)
-    }
+    let anchors = Array.prototype.slice.apply(document.querySelectorAll("a"))
+    let idsDiv = Array.prototype.slice.apply(document.querySelectorAll(".tab-pane"))
 
+    document.getElementById('nav-bar').addEventListener('click', e => {
+        if (e.target.tagName == 'A') {
+            const i = anchors.indexOf(e.target)
+
+            anchors.map(a => a.classList.remove("active"))
+            anchors[i].classList.add("active")
+            idsDiv.map(div => div.classList.remove("active"))
+            idsDiv[i].classList.add("active")
+        }
+    })
+
+})
+
+document.querySelector("form").addEventListener('submit', e => {
+    e.preventDefault()
+    alert(`Thank you for submitting your details ${e.target.name.value}!`)
 })
